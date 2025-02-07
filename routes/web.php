1d34c055e2db27374411082378dce4bf5bf07eb2
steve-ongera <gadafisteve001@gmail.com>
2025-02-08 01:11:03 +0300

    added routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,23 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [ProductController::class, 'index'])->name('home');
+Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
+
+// Cart Routes
+Route::middleware(['auth'])->group(function () {
+    Route::post('/cart/add/{product}', [CartController::class, 'add'])->name('cart.add');
+    Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+    Route::delete('/cart/{id}', [CartController::class, 'remove'])->name('cart.remove');
+    Route::post('/checkout', [OrderController::class, 'checkout'])->name('checkout');
+});
+
+// Admin Routes
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin/products', [AdminController::class, 'index'])->name('admin.products');
+    Route::get('/admin/products/create', [AdminController::class, 'create'])->name('admin.products.create');
+    Route::post('/admin/products', [AdminController::class, 'store'])->name('admin.products.store');
+    Route::get('/admin/products/{id}/edit', [AdminController::class, 'edit'])->name('admin.products.edit');
+    Route::put('/admin/products/{id}', [AdminController::class, 'update'])->name('admin.products.update');
+    Route::delete('/admin/products/{id}', [AdminController::class, 'destroy'])->name('admin.products.destroy');
+});
