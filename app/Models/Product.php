d278e06a1dad698c75d35951c629d7cee6df3c4b
steve-ongera <gadafisteve001@gmail.com>
2025-02-08 01:09:34 +0300

    modified Models for carts, category and product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['name', 'description', 'price', 'image', 'category_id'];
+    
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
