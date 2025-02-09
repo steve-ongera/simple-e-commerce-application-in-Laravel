afb88ee373b0165dd30872d9729e5c23e2095ca2
steve-ongera <gadafisteve001@gmail.com>
2025-02-09 21:03:17 +0300

    modified layouts/auth.blade.php

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-commerce </title>
+     <!-- Favicon -->
+     <link rel="icon" type="image/png" href="{{ asset('logo.png') }}" height=50 , width=50>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
